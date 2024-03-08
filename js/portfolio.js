90d15788853cfaff22885a18a24f80e3f581351f
Jasmine Daniels <create.jasminedaniels@gmail.com>
2024-03-08 18:28:34 -0500

    fixed project feature bug

--- js/portfolio.js
+++ js/portfolio.js
@@ -7,18 +7,27 @@
 
 let projectLinks = document.querySelectorAll('.project-link');
 let projects = document.querySelectorAll('.image-box');
+let lastEl;
 
 console.log("Project Links : ", projectLinks)
 console.log("Projects : ", projects)
 
 projectLinks.forEach((link) => {
     link.addEventListener('mouseover', function(){
+        if(lastEl != null){
+            lastEl.classList.remove('active');
+        }
+        
         for(let i = 0; i < projects.length; i++){
             if(projects[i].getAttribute('name') == link.getAttribute('id')){
                 //projects[i].classList.remove('none');
                 // add an active class that reveals
                 projects[i].classList.toggle('active');
+                lastEl = projects[i];
             } 
         }
     })
 })
+
+
+
